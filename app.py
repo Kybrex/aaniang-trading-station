836a0d3d6ea7f30111e3ec3c419b5450f94928d2
Kybrex <98120594+Kybrex@users.noreply.github.com>
2026-09-04 00:19:46 +0000

    Add one-click complete stock research PDF module

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ import v3_ui
 import v4_ui
 import v5_ui
 import v6_ui
+import v7_ui
 from ui_theme import apply_theme
 
 st.set_page_config(page_title="AANIANG Trading Station", page_icon="S", layout="wide")
@@ -356,3 +357,5 @@ v4_ui.render()
 v5_ui.render()
 
 v6_ui.render()
+
+v7_ui.render()
